776f9f9ecea0b9d2db662eaa89fd5b48c8f57696
Office Laptop <sebghatullah1994@gmail.com>
2024-08-21 18:18:11 +0430

    practice done

--- JS/A Few Miscellaneous JS Features.js
+++ JS/A Few Miscellaneous JS Features.js
@@ -1,53 +1,57 @@
-const greet = (person, greeting = "hi") => {
-  console.log(`${greeting},${person}!`);
-};
+// const greet = (person, greeting = "hi") => {
+//   console.log(`${greeting},${person}!`);
+// };
 
-const blah = (x, y = [1, 2, 3]) => {
-  console.log(x, y);
-};
+// const blah = (x, y = [1, 2, 3]) => {
+//   console.log(x, y);
+// };
 
-const greet = (person, greeting = "Hi", punctuation = "!") => {
-  console.log(`${greeting},${person}${punctuation}`);
-};
+// const greet = (person, greeting = "Hi", punctuation = "!") => {
+//   console.log(`${greeting},${person}${punctuation}`);
+// };
 
-function giveMeFour(a, b, c, d) {
-  console.log("a", a);
-  console.log("a", b);
-  console.log("a", c);
-  console.log("a", d);
-}
+// function giveMeFour(a, b, c, d) {
+//   console.log("a", a);
+//   console.log("a", b);
+//   console.log("a", c);
+//   console.log("a", d);
+// }
 
-const colors = ["red", "orange", "yellow", "green"];
+// const colors = ["red", "orange", "yellow", "green"];
 
-const cephalopods = ["dumbo actopus", "humbolt squid", "flamboyant cuttlefish"];
-const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
-const cnidaria = ["fire coral", "moon jelly"];
-const mollusca = [...cephalopods, ...gastropods];
+// const cephalopods = ["dumbo actopus", "humbolt squid", "flamboyant cuttlefish"];
+// const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
+// const cnidaria = ["fire coral", "moon jelly"];
+// const mollusca = [...cephalopods, ...gastropods];
 
-const feline = {
-  legs: 4,
-  family: "Felidae",
-};
-const canine = {
-  family: "caninae",
-  furry: true,
-  legs: 4,
-};
+// const feline = {
+//   legs: 4,
+//   family: "Felidae",
+// };
+// const canine = {
+//   family: "caninae",
+//   furry: true,
+//   legs: 4,
+// };
 
-const dog = {
-  ...canine,
-  isPet: true,
-  adorable: true,
-};
+// const dog = {
+//   ...canine,
+//   isPet: true,
+//   adorable: true,
+// };
 
-const catDog = {
-  ...canine,
-  ...feline,
-};
+// const catDog = {
+//   ...canine,
+//   ...feline,
+// };
 
-function sum() {
-  const argsArr = [...arguments];
-  return argsArr.reduce((total, currVal) => {
-    return total + currVal;
-  });
-}
+// function sum() {
+//   const argsArr = [...arguments];
+//   return argsArr.reduce((total, currVal) => {
+//     return total + currVal;
+//   });
+// }
+
+const nums = [1, 2, 3, 4, 5, 6];
+
+const [a, b, c] = nums;
