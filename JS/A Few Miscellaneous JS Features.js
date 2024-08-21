f58ae1892eb5c44d17139e05c922991048d3fa9a
Office Laptop <sebghatullah1994@gmail.com>
2024-08-21 17:41:26 +0430

    spread and rest excercised

---
+++ JS/A Few Miscellaneous JS Features.js
@@ -0,0 +1,53 @@
+const greet = (person, greeting = "hi") => {
+  console.log(`${greeting},${person}!`);
+};
+
+const blah = (x, y = [1, 2, 3]) => {
+  console.log(x, y);
+};
+
+const greet = (person, greeting = "Hi", punctuation = "!") => {
+  console.log(`${greeting},${person}${punctuation}`);
+};
+
+function giveMeFour(a, b, c, d) {
+  console.log("a", a);
+  console.log("a", b);
+  console.log("a", c);
+  console.log("a", d);
+}
+
+const colors = ["red", "orange", "yellow", "green"];
+
+const cephalopods = ["dumbo actopus", "humbolt squid", "flamboyant cuttlefish"];
+const gastropods = ["giant african snail", "banana slug", "variable neon slug"];
+const cnidaria = ["fire coral", "moon jelly"];
+const mollusca = [...cephalopods, ...gastropods];
+
+const feline = {
+  legs: 4,
+  family: "Felidae",
+};
+const canine = {
+  family: "caninae",
+  furry: true,
+  legs: 4,
+};
+
+const dog = {
+  ...canine,
+  isPet: true,
+  adorable: true,
+};
+
+const catDog = {
+  ...canine,
+  ...feline,
+};
+
+function sum() {
+  const argsArr = [...arguments];
+  return argsArr.reduce((total, currVal) => {
+    return total + currVal;
+  });
+}
